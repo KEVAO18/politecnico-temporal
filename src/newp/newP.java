14d8e7d3202cbca2f56c031447aa1e820468d186
PCUNO <PCUNO@PC22>
2021-05-22 10:18:28 -0500

    complete any files

---
+++ src/newp/newP.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newp;
+
+/**
+ *
+ * @author PCUNO
+ */
+public class newP {
+
+    public static void main(String[] args) {
+        menu menu = new menu();
+        menu.setVisible(true);
+    }
+}
